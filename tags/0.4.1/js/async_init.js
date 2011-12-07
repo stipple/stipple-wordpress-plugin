6cd409b483b44cf50665588d76b7795f6a59f453
stipple <stipple@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-07 17:57:29 +0000

    version 0.4.1 so wordpress.org will push this version as the new one.
    git-svn-id: http://svn.wp-plugins.org/stipple@472110 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.4.1/js/async_init.js
@@ -0,0 +1,9 @@
+var _stippleq = _stippleq || [];
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = location.protocol === 'https:' ? 'https://stippleit.com/stipple.js' : 'http://stipple.cachefly.net/production/stipple.js';
+  var x = document.getElementsByTagName('script')[0];
+  x.parentNode.insertBefore(s, x);
+})();
