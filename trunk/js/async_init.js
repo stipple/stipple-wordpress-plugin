780dcc6970b5c410da0a6e09ca8f7430ebf5bdf4
stipple <stipple@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-15 00:06:46 +0000

    didn't remove enough to set the src correctly
    git-svn-id: http://svn.wp-plugins.org/stipple@519134 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/js/async_init.js
+++ trunk/js/async_init.js
@@ -3,7 +3,7 @@ var _stippleq = _stippleq || [];
   var s = document.createElement('script');
   s.type = 'text/javascript';
   s.async = true;
-  s.src = location.protocol === '//stipple.cachefly.net/production/stipple.js';
+  s.src = '//stipple.cachefly.net/production/stipple.js';
   var x = document.getElementsByTagName('script')[0];
   x.parentNode.insertBefore(s, x);
 })();
